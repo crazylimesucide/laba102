f3e2611dc46934b5caf62daafee197bba3a3fe06
crazylimesucide <72753034+crazylimesucide@users.noreply.github.com>
2021-01-25 14:27:31 +0200

    Add files via upload

---
+++ laba10.2.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void f1(char* str, int& k)
+{
+	    string path = "t.txt";
+	    ifstream fin;
+	    fin.open(path);
+		while (!fin.eof())
+		{
+			fin.getline(str, 50);
+			cout << str << endl;
+			bool f = true;
+			for (int i = 0; i < strlen(str); i++)
+				if (str[i] == ' ')
+				{
+					f = true;
+				}
+				else
+				{
+					if (f && (str[i+1] != ' ' && str[i+1] != NULL))
+					{
+						k++;
+						f = false;
+					}
+				}
+		}
+		fin.close();
+}
+
+int main()
+{
+	int k = 0;
+	char str[50];
+    f1(str, k);
+	cout << endl;
+	cout << "Kilkist' sliv = " << k << endl;
+
+	return 0;
+}
